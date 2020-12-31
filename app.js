cca4ca983c499a3d22a554968372f759b981cff6
Mohammed Irfan <mohammedirfan412@gmail.com>
2021-01-01 00:30:35 +0530

    Server started

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-console.log('hello')
-console.log('new')
-console.log('hello')
-console.log('new')
-console.log("new")
-console.log('test')
+const http = require('http');
+
+// Create an instance of the http server to handle HTTP requests
+let app = http.createServer((req, res) => {
+    // Set a response type of plain text for the response
+    res.writeHead(200, { 'Content-Type': 'text/plain ' });
+
+    // Send back a response and end the connection
+    res.end('Server started\n');
+});
+
+// Start the server on port 3000
+app.listen(3000, '127.0.0.1');
+console.log('Node server running on port 3000');
